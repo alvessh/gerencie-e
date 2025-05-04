ad1eebb4ba67949cdf8e5bcde809e31e74eabba5
alvessh <117163985+alvessh@users.noreply.github.com>
2025-05-04 09:14:17 -0400

    env name

--- src/main/java/connection/DatabaseConnection.java
+++ src/main/java/connection/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnection {
-	private static final String PROPERTIES_FILE = "/db.properties";
+	private static final String PROPERTIES_FILE = "/env.properties";
 
 	public static Connection getConnection() {
 		try {
